e908433457852e7ecb05637ea36152e3d633e962
T0mer007 <tomerbenaim1@gmail.com>
2023-11-11 16:17:24 +0200

    added search bar

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from './components/Header'
+import Providers from './Providers'
+import NavBar from './components/NavBar'
+import SearchBox from './components/SearchBox'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'IMDB Clone',
+  description: '',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+
+          <Header />
+
+          <NavBar />
+
+          <SearchBox />
+
+          {children}
+
+        </Providers>
+      </body>
+    </html >
+  )
+}
