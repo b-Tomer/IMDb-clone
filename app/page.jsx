96b4053070525bd423a31fb6183e9f5c74d6db43
T0mer007 <tomerbenaim1@gmail.com>
2023-11-11 16:35:54 +0200

    home page data changed

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const API_KEY = process.env.API_KEY
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending"
-  const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&&language=en-US&page=${genre === "fetchTopRated" ? "1" : "2"}`,
+  const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&&language=en-US&page=${genre === "fetchTopRated" ? "2" : "1"}`,
     { next: { revalidate: 10000 } })
 
   if (!res.ok) {
